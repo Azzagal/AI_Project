b6eb46a194532bc88eeb775145d31b60df5e1bf9
Pierre Lorenzen <Pierre.Lorenzen@student.uliege.be>
2024-11-04 14:40:33 +0100

    init

--- project1/hminimax.py
+++ project1/hminimax.py
@@ -16,7 +16,7 @@ class PacmanAgent(Agent):
             state: GameState. See API or class `pacman.GameState`.
         
         Returns:
-            A legal move as defined in `game.Directions`.
+            A legal move as defined in `game.Directions`m.
         """
         legal_moves = state.getLegalActions(0)  # 0 is Pacman's agent index
         
